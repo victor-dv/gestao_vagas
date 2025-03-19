82cfc41dbc560f4d93f92ed779286e71aa2eb95b
victor-dv <js813417@gmail.com>
2025-03-19 00:02:00 -0300

    Formatando mensagens de validações

--- src/main/java/br/com/victor/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.victor.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data 
@@ -9,8 +13,11 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
   private String username;
+  @Email(message = "O campo deve conter um [email] válido")
   private String email;
+  @Length(min = 9, max = 100, message = "A senha deve conter entre 9 e 100 caracteres")
   private String password;
   private String description;
   private String curriculum;
