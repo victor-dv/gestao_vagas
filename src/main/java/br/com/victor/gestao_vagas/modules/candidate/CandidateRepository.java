ed33e6ce57419d28387a6102063c5c5c2fb2fd34
victor-dv <js813417@gmail.com>
2025-03-19 23:39:47 -0300

    Criando o repository e os entity

---
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,10 @@
+package br.com.victor.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+  
+}
